54302b8024892e2a459f3fefd9c32cccda2f0313
badevguru <brian@babastik.com>
2016-03-12 17:54:38 -0800

    Add support for a 4.x kernels

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
 
-if ! [[ "$KERNEL_VERSION" == 4.1.* ]]; then
-  echo "Build only supports 4.1.x Kernels on Debian Jessie"
+if ! [[ "$KERNEL_VERSION" == 4.* ]]; then
+  echo "Build only supports 4.x Kernels on Debian Jessie"
   exit 1
 fi
 
