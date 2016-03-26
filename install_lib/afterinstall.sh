6b2d9632624b7bdd923d742131fef248dbc0eaec
BrianAdams <brian@babastik.com>
2016-03-25 21:35:46 -0700

    install marks if unable to update dumped

--- install_lib/afterinstall.sh
+++ install_lib/afterinstall.sh
@@ -3,7 +3,7 @@ set -x
 set -e
 if [ "$(stat -c %d:%i /)" != "$(stat -c %d:%i /proc/1/root/.)" ]; then
   echo "We are chrooted!"
-  depmod -a
+  touch /.need-depmod
 else
   modprobe -r uvcvideo || true
   depmod -a
